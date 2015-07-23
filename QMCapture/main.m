968a666a747411f68c7f321fb3bf848c3ad7359a
Mr.Liu <3050700400@qq.com>
2015-07-23 20:27:13 +0800

    CREATE

---
+++ QMCapture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QMCapture
+//
+//  Created by 刘永生 on 15/7/23.
+//  Copyright (c) 2015年 刘永生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
